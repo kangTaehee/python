6bb2013f072ba6a37217f4c521cef1d9ed143eac
kang taehee <ilovekth@nate.com>
2022-01-08 04:22:20 +0900

    Update filelist.py
    인자로 받기로 변경, 설명서 추가

--- filelist.py
+++ filelist.py
@@ -1,10 +1,23 @@
+import sys
 from PIL import Image
 import math
 import resize as re
 
 from os import listdir
 from os.path import isfile, join
-mypath = 'C:\python\img\sprite-gnb'
+
+if(len(sys.argv) == 1):
+	print('''설명서 : 
+	1배수 이미지크기를 확인하고 2배수 이미지의 크기를 조정해 드립니다.
+	인자로 png 이미지가 있는 경로를 추가해주세요.
+
+	절대경로 filelist.exe c:/python/asset
+	상대경로 filelist.exe ./asset
+	''')
+	sys.exit()
+
+mypath = sys.argv[1]
+print('입력받은 경로 : ' + mypath)
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 for item in onlyfiles:
@@ -21,3 +34,4 @@ for item in onlyfiles:
 		citem = mypath+'\\'+item
 		re.resize_canvas(citem, citem, old_width, old_height)
 		# filename, new_image_path="save.png", canvas_width=500, canvas_height=500
+
